e7f561800cf2f44fb9e8c0bf56acd7cf07b1ef8b
i-am-shank <shashanks.ug18.ph@nitp.ac.in>
2023-05-28 13:17:46 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+var typeData = new Typed(".role", {
+    strings: [
+        "Frontend Developer",
+        "Programmer",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
